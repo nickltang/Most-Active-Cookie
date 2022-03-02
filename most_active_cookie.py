69627fa7a876731e9849bc6c40bd8626e028f8c5
Nick <nicholasltang21@gmail.com>
2022-03-02 04:32:42 -0800

    Added timestamp formatting to csv parsing

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -1,7 +1,5 @@
 import argparse
 import csv
-import datetime
-from collections import OrderedDict
 
 """
 Command: ./most_active_cookie cookie_log.csv -d 2018-12-09
@@ -10,6 +8,7 @@ Functions:
 - Take CLI arguments for CSV file and date (DONE)
 - Parse CSV file into dictionary (DONE)
 - Cookies already sorted (DONE) -> is sorting necessary?
+- Format timestamp (DONE)
 - Retrieve most active cookies from list
 - 
 """
@@ -24,31 +23,32 @@ def parse_csv_cookies(infile):
     with open(infile) as csv_file:
         csv_reader = csv.reader(csv_file)
         header = next(csv_reader)
-        cookies_dict = OrderedDict({rows[0]:rows[1] for rows in csv_reader})
+        cookies_dict = {rows[0] : format_timestamp(rows[1]) for rows in csv_reader}
     return cookies_dict
 
+# Separate dates from times in CSV timestamps
+def format_timestamp(timestamp):
+    iso_date = (str(timestamp)).split('T')[0]
+    return iso_date
 
 # Search for most active cookie(s) in time frame
-def find_cookie():
+def find_cookie(cookies):
     print('Search for most active cookie')
-
-
-# Separate dates from times in CSV timestamps
-def format_date():
-    print('Formatting date')
+    # Specify by date
+    # Count occurrences of each cookie on date
+    # Get max occurrences of cookies
 
 
 def main():
     parser = argparse.ArgumentParser(description = program_description)
     parser.add_argument('infile', help = infile_help)
-    parser.add_argument('-d', type = datetime.date.fromisoformat, required = True, help = date_help)
+    parser.add_argument('-d', required = True, help = date_help)
 
     # Convert parsed arguments into dictionary
     args = vars(parser.parse_args())    
 
     parsed_cookies = parse_csv_cookies(args['infile'])
-    print(parsed_cookies)
-
+    find_cookie(parsed_cookies)
 
     
 if __name__ == '__main__':
